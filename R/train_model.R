cc707e0378acaf433f28101287c432db7047084e
Núria Estanyol Torres <estanyol.nuria@gmail.com>
2023-08-23 16:33:26 +0200

    Corrected issue with package.

--- R/train_model.R
+++ R/train_model.R
@@ -11,14 +11,14 @@ train_model <- function(train_data) {
     recipes::step_rm(id)
 
   # Create the model specification
-  model_spec <- parsnip::linear_reg(penalty = 1, mixture = 0.5) %>%
+  model_spec <- parsnip::linear_reg(penalty = 0.5, mixture = 0.5) %>%
     parsnip::set_engine("glmnet")
 
   # Train the model
   model <- workflows::workflow() %>%
     workflows::add_recipe(recipe) %>%
     workflows::add_model(model_spec) %>%
-    workflows::fit(data = train_data)
+    parsnip::fit(data = train_data)
 
   return(model$fit)
 }
