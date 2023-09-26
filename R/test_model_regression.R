d962be0a0f9d9dca3024cf22f3f59cd782fae5fe
Daniela Garcia <daniela.ags13@gmail.com>
2023-09-26 14:06:29 +0200

    new ML option, RF

---
+++ R/test_model_regression.R
@@ -0,0 +1,32 @@
+#' test_model_regression
+#'
+#' @param model to be use with the data
+#' @param test_data data to test the model
+#'
+#' @return model coefficients
+#' @export
+#'
+test_model <- function(model, test_data) {
+  #model_fit <- trained_model$fit
+  model_fit <- model$fit
+
+  # Make predictions on the test set
+  predictions <- stats::predict(model_fit, new_data = test_data) %>%
+    dplyr::as_tibble() %>%
+    dplyr::rename(predicted = .pred) %>%
+    dplyr::bind_cols(test_data)
+
+  # Assess model performance
+  rmse <- caret::RMSE(predictions$predicted, predictions$diet_score)
+  # r_squared <- caret::R2(predictions$predicted, predictions$diet_score)
+
+  # View the model coefficients
+  model_coeffs <- broom::tidy(model_fit)
+
+  # Print the evaluation metrics
+  #cat("Root Mean Squared Error:", rmse, "\n")
+  #cat("R-squared:", r_squared, "\n")
+
+  return(list(model_coeffs = model_coeffs, rmse_val = rmse))
+
+}
---
+++ R/train_model_rf.R
@@ -0,0 +1,36 @@
+#' train_model_rf
+#'
+#' @param train_data data to be used for training
+#' @param type two options 1 for regression using linear regression and 2 for classification using random forest.
+#'
+#' @return model parameters
+#' @export
+#'
+train_model_rf <- function(train_data, type = 1) {
+  # Create a recipe
+  recipe <- recipes::recipe(diet_score ~ ., data = train_data) %>%
+    recipes::step_rm(id)
+
+  # Create the model specification
+  if(type == 1){
+    model_spec <- rand_forest() %>%
+      parsnip::set_engine("ranger", importance ='impurity') %>%
+      parsnip::set_mode("regression")
+    #model_spec <- parsnip::linear_reg(penalty = 0.5, mixture = 0.5) %>%
+    #  parsnip::set_engine("glmnet")
+  }else if(type == 2){
+    model_spec <- rand_forest() %>%
+      parsnip::set_engine("ranger", importance ='impurity') %>%
+      parsnip::set_mode("classification")
+  }
+
+  # Train the model
+  model <- workflows::workflow() %>%
+    workflows::add_recipe(recipe) %>%
+    workflows::add_model(model_spec) %>%
+    parsnip::fit(data = train_data)
+
+  return(model$fit)
+  #return(model)
+
+}
