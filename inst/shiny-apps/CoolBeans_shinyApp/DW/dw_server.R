96a32cc336615210bb59c1259219bcf691eeb747
Daniela Garcia <daniela.ags13@gmail.com>
2023-10-03 15:17:50 +0200

    data wrangling implemented

--- inst/shiny-apps/CoolBeans_shinyApp/DW/dw_server.R
+++ inst/shiny-apps/CoolBeans_shinyApp/DW/dw_server.R
@@ -30,8 +30,21 @@ dwServer <- function(id) {
   })})
 
 
-  ###Pre-analitical step###
-   return(original_data)
+  ###Pre-analytical step###
+  observeEvent(input$run, {
+    ncols <- as.numeric(unlist(strsplit(input$ncols,",")))
+    data <- original_data()[,c(ncols[1],ncols[2],ncols[3]:ncol(original_data()))]
+    prep_data <- preprocess_data(data, input$id, input$target, cutoff_met = input$na_cutoff/100, cutoff_subj = input$na_cutoff/100) 
+    
+    #check if something is happening
+    output$preview2 <- renderPrint({
+    skim_without_charts(prep_data,c(1:10))
+    })
+    
+    return(prep_data)
+  })
+
+
 
 
 })}
--- inst/shiny-apps/CoolBeans_shinyApp/DW/dw_ui.R
+++ inst/shiny-apps/CoolBeans_shinyApp/DW/dw_ui.R
@@ -4,9 +4,9 @@ dwUI <- function(id, label = 'DW') {
   tagList(
    #fluidPage(
      titlePanel("Data Wrangling"),
-     p("Here the data will be prepared for the ML step. Run will do the following: 1)remove duplicate columns, 2) remove columns/rows 
+     p("Here the data will be prepared for the ML step. Run will do the following: 1)remove duplicate columns, 2) remove columns/rows
        with high Na number defined by the cutoff value, 3) data imputation using the mean, 4) log transformation and 5) normalization."),
- 
+
      sidebarLayout(
        sidebarPanel(
          wellPanel(
@@ -15,32 +15,34 @@ dwUI <- function(id, label = 'DW') {
            numericInput(ns("columns"), "# columns to include in summary",10),
            actionButton(ns("summary"), "Summary")
          ),
-         
+
          wellPanel(
              title = "Pre-analitical step",
              h4("Do pre-analitical step"),
+             #Columns to select
+             textInput(ns("ncols"), "Enter columns (comma delimited)", "1,2,3"),
              #Insert column names for ID and target
-             textInput(ns("id"), "id column", "Enter text"),
-             textInput(ns("target"), "target column", "Enter text"),
+             textInput(ns("id"), "id column"),
+             textInput(ns("target"), "target column"),
              # columns cutoff
              sliderInput(ns("na_cutoff"), "Na cutoff (%):", min = 0, max = 100, value = 20, step = 10),
              #run the pre-analytical step
              actionButton(ns("run"), "Run")
              )
-         
+
              # wellPanel(
              # title = "Data Splitting",
              # numericInput(ns("train_ratio"), "Training Set Ratio:", 0.7, min = 0.1, max = 0.9, step = 0.1),
              # actionButton(ns("split_data"), "Split"))
        ),
- 
+
        mainPanel(
          # Model Evaluation Outputs
          tabsetPanel(
-           tabPanel("Raw Data Summary", 
+           tabPanel("Raw Data Summary",
                     verbatimTextOutput(ns("preview1"))),
-           tabPanel("Visualization", 
-                    verbatimTextOutput(ns("vis")))
+           tabPanel("Data Pre-Analitical Summary",
+                    verbatimTextOutput(ns("preview2")))
          )
        )
      )
