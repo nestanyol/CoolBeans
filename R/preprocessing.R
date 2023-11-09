4294550a9f8bf6b3b1716c4e9075c6f999151843
Núria Estanyol-Torres <estanyol.nuria@gmail.com>
2023-11-09 15:35:03 +0100

    Deleted old preprocessing function.

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -9,6 +9,7 @@
 #' @param raw_data dataframe with metabolomic data
 #' @param id id of each row
 #' @param target target to focus in the model
+#' @param start_met column where metabolites start
 #' @param cutoff_met threshold to use to discard columns given n NaN
 #' @param cutoff_subj threshold to use to discard rows given n NaN
 #'
@@ -17,9 +18,19 @@
 #'
 #preprocess_data <- function(raw_data, cutoff_met = 0, cutoff_subj = 0) {
 #modified definition to have the cutoffs intended in the comments
-preprocess_data <- function(raw_data, id, target, cutoff_met = 0.2, cutoff_subj = 0.2) {
+preprocess_data_all <- function(raw_data, id, target, start_met, cutoff_met = 0.2, cutoff_subj = 0.2, imputation = "median") {
+    #split data
+    variables <- raw_data[,c(1:start_met-1)]
+    metabolites <- raw_data[,c(start_met:ncol(raw_data))]
+    #get section to use for preprocessing
+    col_selection <- variables %>% dplyr::select(id, target)
+    subdata <- cbind(col_selection, metabolites)
+    #extra variables
+    extra <- variables %>% dplyr::select(!id) %>% dplyr::select(!target)
+
+
   # Remove noise data
-  preprocessed_data_prep <- raw_data %>%
+  preprocessed_data_prep <- subdata %>%
     # Remove rows with NaN in target column
     #tidyr::drop_na(target)%>%
     # Remove duplicate rows
@@ -35,10 +46,28 @@ preprocess_data <- function(raw_data, id, target, cutoff_met = 0.2, cutoff_subj 
   preprocess_recipe <- recipes::recipe(~., data = preprocessed_data_prep) %>%
     #are we always expecting the same column names? #changed to be more general
     recipes::update_role(id, new_role = "id") %>%
-    recipes::update_role(target, new_role = "outcome") %>%
-    # Imputation
-    recipes::step_impute_median(recipes::all_predictors()) %>% #step_imputelower() min?
-    # log transformation
+    recipes::update_role(target, new_role = "outcome")
+
+
+    ### Imputation ###
+    #Give more options for imputation: median, +mean, lower, knn
+    if(imputation == "knn"){
+      preprocess_recipe <- preprocess_recipe %>%
+        recipes::step_impute_knn(recipes::all_predictors())
+    } else if (imputation == "lower"){
+      preprocess_recipe <- preprocess_recipe %>%
+        recipes::step_impute_lower(recipes::all_predictors())
+    } else if (imputation == "mean"){
+      preprocess_recipe <- preprocess_recipe %>%
+      recipes::step_impute_mean(recipes::all_predictors())
+    } else {
+      preprocess_recipe <- preprocess_recipe %>%
+        recipes::step_impute_median(recipes::all_predictors())
+    }
+
+  #continue with transformation and scaling
+  # log transformation
+  preprocess_recipe <- preprocess_recipe %>%
     recipes::step_log(recipes::all_predictors()) %>%
     # normalization
     recipes::step_normalize(recipes::all_predictors())
@@ -52,5 +81,10 @@ preprocess_data <- function(raw_data, id, target, cutoff_met = 0.2, cutoff_subj 
   preprocessed_data <- preprocessed_data%>%rename_at(id, ~'id')
   preprocessed_data <- preprocessed_data%>%rename_at(target, ~'target')
 
-  return(preprocessed_data)
+
+    #join all data
+    preprocessed_dataAll <- cbind(extra, preprocessed_data)
+
+
+  return(preprocessed_dataAll)
 }
