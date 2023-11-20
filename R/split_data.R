643d9760331cdcd7f9fc5243d17a909dead3853d
Luke W. Johnston <lwjohnst@gmail.com>
2023-11-20 22:02:19 +0100

    small typo

--- R/split_data.R
+++ R/split_data.R
@@ -1,14 +1,13 @@
 #' Split dataset
 #'
 #' @description The function splits the data for model training, two datasets
-#' are generated for training and testing. It is possible to define the percetage
+#' are generated for training and testing. It is possible to define the percentage
 #' of data wanted for training, default = 0.7.
-#'
 #'
 #' @param preprocessed_data data after pre-processing
 #' @param training_split data fraction to use for training
 #'
-#' @return split dataframes
+#' @return A list of two [tibble::tibble()] for a testing and training dataset.
 #' @export
 #'
 split_data <- function(preprocessed_data, target, training_split = 0.7) {
