6f26a384fc1564b5f6f514feece161e754624c86
Luke W. Johnston <lwjohnst@gmail.com>
2023-11-20 21:29:29 +0100

    add documentation on the fake dataset

---
+++ R/sim_data.R
@@ -0,0 +1,8 @@
+#' Simulated data mimicking a metabolic network.
+#'
+#' @format This fake dataset, of 2000 rows and 13 columns, contains several variables:
+#'
+#' - `metabolite_`: 12 metabolites with interconnected correlations between some of them, to form a network.
+#' - `diet_score`: A variable that might mimic a dietary score.
+#'
+"sim_data"
