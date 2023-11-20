815d41d678d8c72047691b62f8473980cc733d77
Luke W. Johnston <lwjohnst@gmail.com>
2023-11-20 22:02:26 +0100

    reformatting

--- R/test_model_regression_knn.R
+++ R/test_model_regression_knn.R
@@ -13,22 +13,20 @@
 test_knn_regression <- function(model, test_data) {
   #model_fit <- trained_model$fit
   model_fit <- model$fit
-  
+
   # Make predictions on the test set
   predictions <- stats::predict(model_fit, new_data = test_data) %>%
     dplyr::as_tibble() %>%
     dplyr::rename(predicted = .pred) %>%
     dplyr::bind_cols(test_data)
-  
+
   # Assess model performance
   rmse <- caret::RMSE(predictions$predicted, predictions$target)
   r2 <- caret::R2(predictions$predicted, predictions$target)
-  
+
   # View the variable importance
   #model_importance <- vip::vi(model_fit)
-  
+
   #return(list(model_importance = model_importance, r_squared = r2, RMSE = rmse))
   return(list(prediction = predictions, r_squared = r2, RMSE = rmse))
-  
-  
 }
