32f3dc5591f592ca12c9971e5df4cb0b05e696da
Luke W. Johnston <lwjohnst@gmail.com>
2023-11-20 21:40:17 +0100

    avoid nesting functions within functions

--- R/sing_met.R
+++ R/sing_met.R
@@ -10,28 +10,17 @@
 #' @export
 #'
 #' @examples
-
-
-sing_met <- function(data, exposure_feature, start_met, confounders, threshold=0.1, correction=NULL) {
-
+sing_met <- function(data, exposure_feature, start_met, confounders, threshold = 0.1, correction = NULL) {
   # Variables definition
   metabolite_columns <- colnames(data)[c(start_met:ncol(data))]
-  #defining the model
+  # defining the model
   features <- c(exposure_feature, confounders)
-
-  # Set up modelling function:
-  lm_singmet <- function(data, metabolite, y) {
-    model_formula <- reformulate(features, response = metabolite)
-    results <- lm(model_formula, data = data)
-    broom::tidy(results) %>%
-      mutate(yvar = metabolite,.before = everything())
-  }
 
   # Set up parallel processing
   plan(multisession)
 
   output <- metabolite_columns %>%
-    furrr::future_map(~lm_singmet(.x, features, data=data)) %>%
+    furrr::future_map(~ lm_singmet(.x, features, data = data)) %>%
     list_rbind(names_to = "model_id")
 
   # Reset the plan to sequential
@@ -41,14 +30,22 @@ sing_met <- function(data, exposure_feature, start_met, confounders, threshold=0
     p.value_corrected <- p.adjust(output$p.value, method = correction)
     output <- cbind(output, p.value_corrected)
 
-    output_filtered <- output[str_detect(output$term, "target"),] %>%
+    output_filtered <- output[str_detect(output$term, "target"), ] %>%
       filter(p.value_corrected < threshold) %>%
       arrange(p.value_corrected)
     return(output_filtered)
   } else {
-    output_filtered <- output[str_detect(output$term, "target"),] %>%
+    output_filtered <- output[str_detect(output$term, "target"), ] %>%
       filter(p.value < threshold) %>%
       arrange(p.value)
     return(output_filtered)
-    }
+  }
+}
+
+# Set up modelling function:
+lm_singmet <- function(data, metabolite, y) {
+  model_formula <- reformulate(features, response = metabolite)
+  results <- lm(model_formula, data = data)
+  broom::tidy(results) %>%
+    mutate(yvar = metabolite, .before = everything())
 }
