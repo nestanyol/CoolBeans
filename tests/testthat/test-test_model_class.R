c7f8402cd43a4875226cff592a728ff881f030bb
danielaags <daniela.ags13@gmail.com>
2023-11-24 15:34:37 +0100

    test functions for random forest model: train and test

---
+++ tests/testthat/test-test_model_class.R
@@ -0,0 +1,21 @@
+library(dplyr)
+
+test_that("Check the expected rmse value from random forest model using classification", {
+  #evaluates the model and evaluates the rmse value
+  data <- sim_data |>
+    preprocessing(id = "id", target = "sex", start_metabolites = 5) |>
+    select(id, target, starts_with("met")) |>
+    drop_na(target) |>
+    splitting()
+
+  trained_model <- training_rf(data$train_data, type = 2)
+
+  tested_model <- testing_rf_classification(trained_model, data$test_data)
+
+  actual <- pull(actual$model_importance["Importance"][1,1])
+
+  # We expect an approximate rmse value.
+  expected <- 45
+
+  expect_equal(actual, expected, tolerance = 5)
+})
