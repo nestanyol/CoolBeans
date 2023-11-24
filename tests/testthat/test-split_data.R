865d2d6c8a2cfc831cc968c7bbe9b950821713e2
danielaags <daniela.ags13@gmail.com>
2023-11-24 12:44:47 +0100

    test splitting function

---
+++ tests/testthat/test-split_data.R
@@ -0,0 +1,11 @@
+test_that("Evaluates output of two data frames", {
+  actual <- sim_data |>
+    preprocessing(id = "id", target = "exposure", start_metabolites = 4) |>
+    splitting() |>
+    length()
+
+  # We expect the standardization to have 12 zeroes.
+  expected <- 2
+
+  expect_equal(actual, expected)
+})
