0c1040e68d8d9039e498996664c05b68f4126017
Núria Estanyol-Torres <estanyol.nuria@gmail.com>
2023-11-24 10:45:19 +0100

    Solved merging conflicts and run styler

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -17,27 +17,22 @@
 #' and column target.
 #' @export
 #'
-<<<<<<< HEAD
-preprocess_data <- function(raw_data, id, target, cutoff_met = 0.2, cutoff_subj = 0.2) {
-=======
-# preprocess_data <- function(raw_data, cutoff_met = 0, cutoff_subj = 0) {
-# modified definition to have the cutoffs intended in the comments
 preprocessing <- function(raw_data, id, target, start_metabolites, cutoff_columns = 0.2, cutoff_rows = 0.2, imputation = "median") {
-  #split data
-  variables <- raw_data[,c(1:start_metabolites-1)]
-  metabolites <- raw_data[,c(start_metabolites:ncol(raw_data))]
-  #get section to use for preprocessing
+  # split data
+  variables <- raw_data[, c(1:start_metabolites - 1)]
+  metabolites <- raw_data[, c(start_metabolites:ncol(raw_data))]
+  # get section to use for preprocessing
   col_selection <- variables %>% dplyr::select(id, target)
   subdata <- cbind(col_selection, metabolites)
-  #extra variables
-  extra <- variables %>% dplyr::select(!id) %>% dplyr::select(!target)
+  # extra variables
+  extra <- variables %>%
+    dplyr::select(!id) %>%
+    dplyr::select(!target)
 
-
->>>>>>> d2fe28a388d39fe58ceac6dab067345ed0177825
   # Remove noise data
   preprocessed_data_prep <- subdata %>%
     # Remove rows with NaN in target column
-    #tidyr::drop_na(target)%>%
+    tidyr::drop_na(target) %>%
     # Remove duplicate rows
     dplyr::distinct() %>%
     # Remove columns (metabolites) that have 20% of values as missing
@@ -45,24 +40,24 @@ preprocessing <- function(raw_data, id, target, start_metabolites, cutoff_column
     # Remove rows (subjects) that have 20% of values as missing
     janitor::remove_empty("rows", cutoff = cutoff_rows) # Optional step
 
-  #some data sets migth have NaN in the target column, can we do something about it during this step?
+  # some data sets migth have NaN in the target column, can we do something about it during this step?
 
   # Create a recipe for preprocessing the data
   preprocess_recipe <- recipes::recipe(~., data = preprocessed_data_prep) %>%
-    #are we always expecting the same column names? #changed to be more general
+    # are we always expecting the same column names? #changed to be more general
     recipes::update_role(id, new_role = "id") %>%
     recipes::update_role(target, new_role = "outcome")
 
 
   ### Imputation ###
-  #Give more options for imputation: median, +mean, lower, knn
-  if(imputation == "knn"){
+  # Give more options for imputation: median, +mean, lower, knn
+  if (imputation == "knn") {
     preprocess_recipe <- preprocess_recipe %>%
       recipes::step_impute_knn(recipes::all_predictors())
-  } else if (imputation == "lower"){
+  } else if (imputation == "lower") {
     preprocess_recipe <- preprocess_recipe %>%
       recipes::step_impute_lower(recipes::all_predictors())
-  } else if (imputation == "mean"){
+  } else if (imputation == "mean") {
     preprocess_recipe <- preprocess_recipe %>%
       recipes::step_impute_mean(recipes::all_predictors())
   } else {
@@ -70,7 +65,7 @@ preprocessing <- function(raw_data, id, target, start_metabolites, cutoff_column
       recipes::step_impute_median(recipes::all_predictors())
   }
 
-  #continue with transformation and scaling
+  # continue with transformation and scaling
   # log transformation
   preprocess_recipe <- preprocess_recipe %>%
     recipes::step_log(recipes::all_predictors()) %>%
@@ -82,13 +77,12 @@ preprocessing <- function(raw_data, id, target, start_metabolites, cutoff_column
     recipes::prep(data = preprocessed_data_prep) %>%
     recipes::juice()
 
-  #allows the downstream funtions to run with this initial decision
-  preprocessed_data <- preprocessed_data%>%rename_at(id, ~'id')
-  preprocessed_data <- preprocessed_data%>%rename_at(target, ~'target')
+  # allows the downstream funtions to run with this initial decision
+  preprocessed_data <- preprocessed_data %>% rename_at(id, ~"id")
+  preprocessed_data <- preprocessed_data %>% rename_at(target, ~"target")
 
-
-  #join all data
+  # join all data
   preprocessed_data_all <- cbind(extra, preprocessed_data)
 
-  #return(preprocessed_data)
+  # return(preprocessed_data)
 }
