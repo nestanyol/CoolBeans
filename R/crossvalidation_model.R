14187174fcf3c7ede4f23bb23766c68bc015bbd2
Daniela Garcia <daniela.ags13@gmail.com>
2024-01-03 14:57:44 +0100

    model training with crossvalidation

---
+++ R/crossvalidation_model.R
@@ -0,0 +1,37 @@
+#' Model training using cross-validation and the model of choice
+#'
+#' @description The function trains a model using linear regression (elastic net)
+#' and cross-validation
+#'
+#'
+#' @param train_data data to be used for training
+#' @param model model to be used, default is elastic model
+#' @param nfolds number of folds to be used
+#' @param nrepeats number of repeats to be used
+#' @param ltune tuneLength parameter during training
+#'
+#' @return Output from training model
+#' @export
+#'
+crossvalidation_model <- function(train_data, model = "glmnet", nfolds=2, nrepeats=2, ltune=5) {
+  #split data
+  #train_data = data$train_data
+  #test_data = data$test_data
+
+  control <- caret::trainControl(method = "repeatedcv",
+                          number = nfolds, #number of folds
+                          repeats = nrepeats, #number of repeats
+                          search = "random",
+                          verboseIter = TRUE)
+
+  # Training Elastic Net Regression model
+  set.seed(42) #set.seed to make it reproducible (otherwise different result each time)
+  model <- caret::train(target~ .,
+                         data = train_data,
+                         method = model, #method to use
+                         preProcess = c("center", "scale"),
+                         tuneLength = ltune, # grid granularity
+                         trControl = control)
+
+  model
+}
